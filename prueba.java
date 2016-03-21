5af03df6954cdcc2c961f3da28a94c4fd896de0b
Guillermo Lapuente <glapuentevalea@gmail.com>
2016-03-21 15:03:31 +0100

    Changed the java

--- prueba.java
+++ prueba.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello,  HOLA  World");
+        System.out.println("Hello, HALO  HOLA  World");
     }
 
 }
